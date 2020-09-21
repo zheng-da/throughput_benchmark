313d8e4e42c13711093f6911ddb5a15bdee5030e
Ubuntu <ubuntu@ip-172-31-26-148.us-west-2.compute.internal>
2020-09-21 19:01:06 +0000

    fix.

--- pcie_throughput.py
+++ pcie_throughput.py
@@ -3,7 +3,7 @@ import numpy as np
 import time
 
 # The number of GPUs we'll copy data to simultaneously.
-num_gpus = 1
+num_gpus = 8
 # The size of the array that we copy to GPUs.
 arr_size = 1000000
 
@@ -11,9 +11,12 @@ arr = th.randn(arr_size, 10)
 arr = arr.pin_memory()
 devs = []
 gpu_arrs = []
+pinned_arrs = []
 for i in range(num_gpus):
     dev = th.device('cuda:' + str(i))
     gpu_arrs.append(th.randn(arr_size, 10, device=dev))
+    pinned_arrs.append(th.zeros(arr_size, 10, pin_memory=True))
+    pinned_arrs[-1][:] = 0
     devs.append(dev)
 cpu_dev = th.device('cpu')
 
@@ -24,8 +27,20 @@ for i in range(10):
         #arr1 = arr.to(dev)
         arr1 = arr.to(dev, non_blocking=True)
         res.append(arr1)
-    for arr in gpu_arrs:
-        res.append(arr.to(cpu_dev, non_blocking=True))
     th.cuda.synchronize()
     seconds = time.time() - start
-    print('to {} GPUs: {:.3f}'.format(len(devs), np.prod(arr.shape) * 4 * 2 / seconds / 1000000000 * len(devs)))
+    print('to {} GPUs: {:.3f}'.format(len(devs), np.prod(arr.shape) * 4 / seconds / 1000000000 * len(devs)))
+
+for i in range(10):
+    start = time.time()
+    res = []
+    for dev in devs:
+        #arr1 = arr.to(dev)
+        arr1 = arr.to(dev, non_blocking=True)
+        res.append(arr1)
+    for i, arr in enumerate(gpu_arrs):
+        pinned_arrs[i].copy_(arr, non_blocking=True)
+        #res.append(arr.to(cpu_dev, non_blocking=True))
+    th.cuda.synchronize()
+    seconds = time.time() - start
+    print('from/to {} GPUs: {:.3f}'.format(len(devs), np.prod(arr.shape) * 4 * 2 / seconds / 1000000000 * len(devs)))
