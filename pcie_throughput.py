ea6851d94c97b19b0a1d19062e06a979f08084f9
Ubuntu <ubuntu@ip-172-31-26-148.us-west-2.compute.internal>
2020-09-21 17:52:31 +0000

    fix.

--- pcie_throughput.py
+++ pcie_throughput.py
@@ -5,13 +5,17 @@ import time
 # The number of GPUs we'll copy data to simultaneously.
 num_gpus = 1
 # The size of the array that we copy to GPUs.
-arr_size = 100000
+arr_size = 1000000
 
 arr = th.randn(arr_size, 10)
 arr = arr.pin_memory()
 devs = []
+gpu_arrs = []
 for i in range(num_gpus):
-    devs.append(th.device('cuda:' + str(i)))
+    dev = th.device('cuda:' + str(i))
+    gpu_arrs.append(th.randn(arr_size, 10, device=dev))
+    devs.append(dev)
+cpu_dev = th.device('cpu')
 
 for i in range(10):
     start = time.time()
@@ -20,6 +24,8 @@ for i in range(10):
         #arr1 = arr.to(dev)
         arr1 = arr.to(dev, non_blocking=True)
         res.append(arr1)
+    for arr in gpu_arrs:
+        res.append(arr.to(cpu_dev, non_blocking=True))
     th.cuda.synchronize()
     seconds = time.time() - start
-    print('to {} GPUs: {:.3f}'.format(len(devs), np.prod(arr.shape) * 4 / seconds / 1000000000 * len(devs)))
+    print('to {} GPUs: {:.3f}'.format(len(devs), np.prod(arr.shape) * 4 * 2 / seconds / 1000000000 * len(devs)))
